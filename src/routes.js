cccc4e644349414735c8b3f9873bc9faf88e7114
Gustavo <graf.sousa@gmail.com>
2019-10-16 19:26:50 -0300

    Altera importações

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
@@ -6,4 +6,6 @@ routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
-module.exports = routes;
+export {
+  routes,
+};
